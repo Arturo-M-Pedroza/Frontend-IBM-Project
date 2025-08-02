f029959084eb335e826c34906e0eda2e59baa92e
Arturo Morales Pedroza <arturo.m.pedroza@gmail.com>
2025-08-02 14:21:45 -0400

    Update of vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
 
-    base:"/Frontend-IMB-Project",
+    base:"/learning_react",
   plugins: [react()],
 
 })
